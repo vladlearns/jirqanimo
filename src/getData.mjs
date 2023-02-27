6ac4e26b74c58300740064041df0562dd7ac5781
vladlearns <vladlearns@gmail.com>
2023-02-27 12:07:57 +0200

    fix: various fixes

--- src/getData.mjs
+++ src/getData.mjs
@@ -26,28 +26,86 @@ async function getIssues(jql) {
 async function getFilteredIssues(
 	status,
 	label,
+	issueType,
 	sortField = "created",
 	sortOrder = "DESC",
 	sprint = null
 ) {
 	const query =
-		`status = "${status}"` +
-		(label ? ` AND labels = "${label}"` : "") +
-		(sprint ? ` AND Sprint = "${sprint}"` : "");
+		`status ${status}` +
+		(label ? ` AND labels ${label}` : "") +
+		(issueType ? ` AND issuetype ${issueType}` : "") +
+		(sprint ? ` AND sprint ${sprint}` : "");
 	const sortQuery = `order by ${sortField} ${sortOrder}`;
-	const fullQuery = label ? `${query} ${sortQuery}` : query;
+	const fullQuery = `${query} ${sortQuery}`;
 	const issues = await getIssues(fullQuery);
 	return issues;
 }
 
-const openedStories = await getFilteredIssues("To Do");
-const closedStories = await getFilteredIssues("Done");
-const seoBugs = await getFilteredIssues("To Do", "SEO_BUG");
-const uiBugs = await getFilteredIssues("To Do", "UI_BUG");
-const functionalBugs = await getFilteredIssues("To Do", "FUNCTIONAL_BUG");
-const translationBugs = await getFilteredIssues("To Do");
-const notFixedBugs = await getFilteredIssues("To Do");
-const leakedBugs = await getFilteredIssues("To Do");
+const openedStories = await getFilteredIssues(
+	"NOT IN (Done)",
+	null,
+	"= Story",
+	"created",
+	"ASC",
+	"=353"
+);
+const closedStories = await getFilteredIssues(
+	"='Done'",
+	null,
+	"= Story",
+	"created",
+	"ASC",
+	"=353"
+);
+const seoBugs = await getFilteredIssues(
+	"='Open'",
+	"= SEO_bug",
+	"in (Subtask, Bug)",
+	"created",
+	"ASC",
+	"=353"
+);
+const uiBugs = await getFilteredIssues(
+	"='Open'",
+	"= UI_bug",
+	"in (Subtask, Bug)",
+	"created",
+	"ASC",
+	"=353"
+);
+const functionalBugs = await getFilteredIssues(
+	"='Open'",
+	"= Functional_Bug",
+	"in (Subtask, Bug)",
+	"created",
+	"ASC",
+	"=353"
+);
+const translationBugs = await getFilteredIssues(
+	"='Open'",
+	"= Translation_bug",
+	"in (Subtask, Bug)",
+	"created",
+	"ASC",
+	"=353"
+);
+const notFixedBugs = await getFilteredIssues(
+	"NOT IN (Done)",
+	null,
+	"in (Subtask, Bug)",
+	"created",
+	"ASC",
+	"=353"
+);
+const leakedBugs = await getFilteredIssues(
+	"NOT IN (Done)",
+	null,
+	"in (Subtask, Bug)",
+	"created",
+	"ASC",
+	"=353"
+);
 
 const stats = {
 	openedStories,
@@ -85,48 +143,46 @@ const data = [
 		name: "SEO Bugs",
 		count: seoBugs.total,
 		date: currentDate,
-		issues: collectIssues(closedStories),
+		issues: collectIssues(seoBugs),
 	},
 	{
 		name: "UI Bugs",
 		count: uiBugs.total,
 		date: currentDate,
-		issues: collectIssues(closedStories),
+		issues: collectIssues(uiBugs),
 	},
 	{
 		name: "Functional Bugs",
 		count: functionalBugs.total,
 		date: currentDate,
-		issues: collectIssues(closedStories),
+		issues: collectIssues(functionalBugs),
 	},
 	{
 		name: "Translation Bugs",
 		count: translationBugs.total,
 		date: currentDate,
-		issues: collectIssues(closedStories),
+		issues: collectIssues(translationBugs),
 	},
 	{
 		name: "Not Fixed Bugs",
 		count: notFixedBugs.total,
 		date: currentDate,
-		issues: collectIssues(closedStories),
+		issues: collectIssues(notFixedBugs),
 	},
 	{
 		name: "Leaked Bugs",
 		count: leakedBugs.total,
 		date: currentDate,
-		issues: collectIssues(closedStories),
+		issues: collectIssues(leakedBugs),
 	},
 ];
 const options = {
 	fields: ["name", "count", "date", "issues"],
 };
 
-// Convert the data to a CSV string
 const parser = new Parser(options);
 const csv = parser.parse(data);
 
-// Write the CSV string to a file
 fs.writeFile("./data.csv", csv, (err) => {
 	if (err) {
 		console.error(err);
